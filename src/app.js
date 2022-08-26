24c84c526b28c8c03a069cfa17e3397947ed00cb
Francksuel <francksuelbarbosa@gmail.com>
2022-08-26 17:54:48 -0300

    Post Login

--- src/app.js
+++ src/app.js
@@ -2,3 +2,20 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+const tweets=[];
+const users=[];
+app.post('/sign-up',(req,res)=>{
+    const newUser = req.body;
+    console.log(req);
+    users.push(newUser);
+    res.send(users);
+});
+app.get('/',(req,res)=>{
+    res.send('Olá');
+});
+
+
+app.listen(5000,()=>console.log('Listining on port 5000'));
