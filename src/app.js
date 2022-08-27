4c60c0e81a5991b0b16a74bd0de680e620394df3
Francksuel <francksuelbarbosa@gmail.com>
2022-08-27 18:51:39 -0300

    Completed requirements

--- src/app.js
+++ src/app.js
@@ -8,14 +8,21 @@ app.use(express.json());
 const tweets=[];
 const users=[];
 app.post('/sign-up',(req,res)=>{
-    const newUser = req.body;
-    console.log(req);
+    const newUser = req.body;   
     users.push(newUser);
-    res.send(users);
+    res.send("OK");
 });
-app.get('/',(req,res)=>{
-    res.send('Olá');
+app.post('/tweets',(req,res)=>{
+    const newTweet = req.body;
+    const user = users.find(user => user.username === req.body.username)    
+    tweets.push({...newTweet, 
+        avatar:user.avatar});
+    res.send("OK");
 });
 
+app.get('/tweets',(req,res)=>{
+    const recentTweets = tweets.slice(-10);
+    res.send(recentTweets);
+});
 
 app.listen(5000,()=>console.log('Listining on port 5000'));
