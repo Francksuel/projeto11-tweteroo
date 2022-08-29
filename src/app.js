3c3d08fcf37aef3fe86e3ac5f8bc5d1ad6ec87c1
Francksuel <francksuelbarbosa@gmail.com>
2022-08-29 14:50:32 -0300

    User tweets return

--- src/app.js
+++ src/app.js
@@ -7,24 +7,39 @@ app.use(express.json());
 
 const tweets = [];
 const users = [];
+
+function imageVerification(avatar) {
+	try {
+		const url = new URL(avatar);
+		return true;
+	} catch (error) {
+		return false;
+	}
+}
+
 app.post("/sign-up", (req, res) => {
 	const { username, avatar } = req.body;
 	if (!username || !avatar) {
 		res.status(400).send("Todos os campos são obrigatórios!");
 		return;
 	}
+    if (!imageVerification(avatar)){
+        res.status(400).send("Envie uma URL válida");
+		return;
+    }
 	users.push({ username, avatar });
 
 	res.status(201).send({ username, avatar });
 });
+
 app.post("/tweets", (req, res) => {
 	const { tweet } = req.body;
-	const username = req.headers.user;	
+	const username = req.headers.user;
 	if (!username || !tweet) {
 		res.status(400).send("Todos os campos são obrigatórios!");
 		return;
 	}
-	const user = users.find((userLogged) => userLogged.username === username);	
+	const user = users.find((userLogged) => userLogged.username === username);
 	tweets.push({ tweet, username, avatar: user.avatar });
 	res.status(201).send({ username, avatar: user.avatar, tweet });
 });
@@ -32,6 +47,12 @@ app.post("/tweets", (req, res) => {
 app.get("/tweets", (req, res) => {
 	const recentTweets = tweets.slice(-10);
 	res.send(recentTweets);
+});
+
+app.get("/tweets/:username", (req, res) => {
+    const username = req.params.username;
+    const tweetsUser = tweets.filter((userLogged) => userLogged.username === username);	
+	res.send(tweetsUser);
 });
 
 app.listen(5000, () => console.log("Listining on port 5000"));
