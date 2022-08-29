6ada5936e57b22aaef956f5770575bda19cf4155
Francksuel <francksuelbarbosa@gmail.com>
2022-08-29 14:31:02 -0300

    Validated tweet data

--- src/app.js
+++ src/app.js
@@ -1,33 +1,37 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const tweets=[];
-const users=[];
-app.post('/sign-up',(req,res)=>{
-    const {username, avatar} = req.body;   
-    if (!username || !avatar){
-        res.status(400).send("Todos os campos são obrigatórios!");
-        return
-    }
-    users.push({username,avatar});
+const tweets = [];
+const users = [];
+app.post("/sign-up", (req, res) => {
+	const { username, avatar } = req.body;
+	if (!username || !avatar) {
+		res.status(400).send("Todos os campos são obrigatórios!");
+		return;
+	}
+	users.push({ username, avatar });
 
-    res.send("OK");
+	res.status(201).send({ username, avatar });
 });
-app.post('/tweets',(req,res)=>{
-    const newTweet = req.body;
-    const user = users.find(user => user.username === req.body.username)    
-    tweets.push({...newTweet, 
-        avatar:user.avatar});
-    res.send("OK");
+app.post("/tweets", (req, res) => {
+	const { tweet } = req.body;
+	const username = req.headers.user;	
+	if (!username || !tweet) {
+		res.status(400).send("Todos os campos são obrigatórios!");
+		return;
+	}
+	const user = users.find((userLogged) => userLogged.username === username);	
+	tweets.push({ tweet, username, avatar: user.avatar });
+	res.status(201).send({ username, avatar: user.avatar, tweet });
 });
 
-app.get('/tweets',(req,res)=>{
-    const recentTweets = tweets.slice(-10);
-    res.send(recentTweets);
+app.get("/tweets", (req, res) => {
+	const recentTweets = tweets.slice(-10);
+	res.send(recentTweets);
 });
 
-app.listen(5000,()=>console.log('Listining on port 5000'));
+app.listen(5000, () => console.log("Listining on port 5000"));
